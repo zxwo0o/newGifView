3c3b3ae94035f6727b0ce433e76e1c84626ebdd0
zxwo0o <zxwo0o@hotmail.com>
2013-01-23 02:06:14 -0800

    Create GifView.m

---
+++ GifView.m
@@ -0,0 +1,69 @@
+
+#import "GifView.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation GifView
+
+
+- (id)initWithFrame:(CGRect)frame filePath:(NSString *)_filePath{
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+  	gifProperties = [[NSDictionary dictionaryWithObject:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:0] forKey:(NSString *)kCGImagePropertyGIFLoopCount]
+													 forKey:(NSString *)kCGImagePropertyGIFDictionary] retain];
+		gif = CGImageSourceCreateWithURL((CFURLRef)[NSURL fileURLWithPath:_filePath], (CFDictionaryRef)gifProperties);
+		count =CGImageSourceGetCount(gif);
+		timer = [NSTimer scheduledTimerWithTimeInterval:0.12 target:self selector:@selector(play) userInfo:nil repeats:YES];
+		[timer fire];
+    }
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame data:(NSData *)_data{
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+		gifProperties = [[NSDictionary dictionaryWithObject:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:0] forKey:(NSString *)kCGImagePropertyGIFLoopCount]
+													 forKey:(NSString *)kCGImagePropertyGIFDictionary] retain];
+        //		gif = CGImageSourceCreateWithURL((CFURLRef)[NSURL fileURLWithPath:_filePath], (CFDictionaryRef)gifProperties);
+        gif = CGImageSourceCreateWithData((CFDataRef)_data, (CFDictionaryRef)gifProperties);
+		count =CGImageSourceGetCount(gif);
+		timer = [NSTimer scheduledTimerWithTimeInterval:0.12 target:self selector:@selector(play) userInfo:nil repeats:YES];
+		[timer fire];
+    }
+    return self;
+}
+
+-(void)updateGifView:(NSString *)_filePath {
+    
+    gif = CGImageSourceCreateWithURL((CFURLRef)[NSURL fileURLWithPath:_filePath], (CFDictionaryRef)gifProperties);
+    count =CGImageSourceGetCount(gif);
+    
+    [self play];
+}
+
+
+-(void)play
+{
+	index ++;
+	index = index%count;
+	CGImageRef ref = CGImageSourceCreateImageAtIndex(gif, index, (CFDictionaryRef)gifProperties);
+	self.layer.contents = (id)ref;
+    CFRelease(ref);
+}
+-(void)removeFromSuperview
+{
+	NSLog(@"removeFromSuperview");
+	[timer invalidate];
+	timer = nil;
+    
+	[super removeFromSuperview];
+}
+- (void)dealloc {
+    NSLog(@"dealloc");
+	CFRelease(gif);
+	[gifProperties release];
+    [super dealloc];
+}
+@end
